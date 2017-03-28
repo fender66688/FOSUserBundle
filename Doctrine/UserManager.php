b26a46a684997f39fa52e5f39f24ef8c8b98c349
Lukas Kahwe Smith <smith@pooteeweet.org>
2017-03-28 13:27:49 +0200

    make the UserManager non lazy

--- Doctrine/UserManager.php
+++ Doctrine/UserManager.php
@@ -12,7 +12,6 @@
 namespace FOS\UserBundle\Doctrine;
 
 use Doctrine\Common\Persistence\ObjectManager;
-use Doctrine\Common\Persistence\ObjectRepository;
 use FOS\UserBundle\Model\UserInterface;
 use FOS\UserBundle\Model\UserManager as BaseUserManager;
 use FOS\UserBundle\Util\CanonicalFieldsUpdater;
